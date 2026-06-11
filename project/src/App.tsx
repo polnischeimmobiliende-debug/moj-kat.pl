c455d960d3d06f0f1c47305b4215cbb9c5aa348d
polnischeimmobiliende-debug <polnische.immobilien.de@gmail.com>
2026-06-11 09:01:57 +0200

    Updated App.tsx

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -5,23 +5,30 @@ import { SearchPage } from './pages/SearchPage';
 import { PropertyPage } from './pages/PropertyPage';
 import { LoginPage } from './pages/LoginPage';
 import { RegisterPage } from './pages/RegisterPage';
+import { DashboardPage } from './pages/DashboardPage';
+import { AddPropertyPage } from './pages/AddPropertyPage';
 
 import { AuthProvider } from './hooks/useAuth';
 
 export default function App() {
-  return (
-    <AuthProvider>
-      <Layout>
-        <Route pattern="/" component={() => <SearchPage />} />
+return ( <AuthProvider> <Layout>
+<Route pattern="/" component={() => <SearchPage />} />
 
-        <Route
-          pattern="/property/:id/:slug"
-          component={({ id }) => <PropertyPage id={id} />}
-        />
+```
+    <Route
+      pattern="/property/:id/:slug"
+      component={({ id }) => <PropertyPage id={id} />}
+    />
 
-        <Route pattern="/login" component={() => <LoginPage />} />
-        <Route pattern="/register" component={() => <RegisterPage />} />
-      </Layout>
-    </AuthProvider>
-  );
+    <Route pattern="/login" component={() => <LoginPage />} />
+    <Route pattern="/register" component={() => <RegisterPage />} />
+
+    <Route pattern="/dashboard" component={() => <DashboardPage />} />
+
+    <Route pattern="/add" component={() => <AddPropertyPage />} />
+  </Layout>
+</AuthProvider>
+```
+
+);
 }
